69bac9684054a20e4c73ba884f55a49dd76ab739
Lio <liobensignor@gmail.com>
2024-04-29 19:15:25 -0300

    70% de Logica errores + correcciones

--- content/js/vuelo-hotel.js
+++ content/js/vuelo-hotel.js
@@ -82,49 +82,138 @@ function cambiarEstrellas(event) {
   }
 }
 
-function isValidEmail(email) {
-  return /\S+@\S+\.\S+/.test(email);
+function mostrarError(elemento, mensaje) {
+  const errorIcon = document.createElement('img');
+  errorIcon.src = "/content/img/error-icon.png";
+  errorIcon.className = 'errorIcon';
+  const errorText = document.createElement('p');
+  errorText.className = 'error';
+  errorText.textContent = mensaje;
+  elemento.innerHTML = '';
+  elemento.appendChild(errorIcon);
+  elemento.appendChild(errorText);
 }
+
+function validarNombre(nombre, errorNombre) {
+  if (nombre.trim() === '') {
+    mostrarError(errorNombre, 'El nombre es requerido');
+    return false;
+  } else if (nombre.trim().length >= 20) {
+    mostrarError(errorNombre, 'El nombre debe tener menos de 20 caracteres');
+    return false;
+  } else if (nombre.trim().length < 2) {
+    mostrarError(errorNombre, 'El nombre de tener al menos 2 caracteres');
+    return false;
+  } else {
+    errorNombre.innerHTML = '';
+    return true;
+  }
+}
+
+function validarPaisResidencia(paisResidencia, errorPaisResidencia) {
+  if (paisResidencia === 'Selecciona tu país') {
+    mostrarError(errorPaisResidencia, 'Elige un país')
+  } else {
+    errorPaisResidencia.innerHTML = '';
+    return true;
+  }
+}
+
+function validarMail(mail, errorMail) {
+  if (mail.trim() === '') {
+    mostrarError(errorMail, 'El mail es requerido');
+    return false;
+  } else if (!/.+@.+\..+/.test(mail)) {
+    mostrarError(errorMail, 'El mail es inválido');
+    return false;
+  } else {
+    errorMail.innerHTML = '';
+    return true;
+  }
+}
+
+function validarNumeroWhatsapp(numeroWhatsapp, area, errorWhatsapp){
+  if (numeroWhatsapp.trim() === '' || area.trim() === '') {
+    mostrarError(errorWhatsapp, 'El número y área son requeridos');
+    return false;
+  } else {
+    errorWhatsapp.innerHTML = '';
+    return true;
+  }
+}
+
+function validarFechas(fechaSalida, fechaVuelta, errorFechas){
+  if (fechaSalida.trim() === '' || fechaVuelta.trim() === '') {
+    mostrarError(errorFechas, 'La fechas son requeridas');
+    return false;
+  } else if (fechaSalida.trim() ===  fechaVuelta.trim()) {
+    mostrarError(errorFechas, 'La fechas no pueden ser iguales');
+    return false;
+    // AGREGAR QUE VUELTA TIENE QUE SER POSTERIOR A SALIDA
+  } else {
+    errorFechas.innerHTML = '';
+    return true;
+  }
+}
+
+function validarFormulario() {
+  const nombre = document.getElementById('nombre').value;
+  const errorNombre = document.getElementById('errorNombre');
+  const paisResidencia = document.getElementById('paisResidencia').value;
+  const errorPaisResidencia = document.getElementById('errorPaisResidencia');
+  const numeroWhatsapp = document.getElementById('numeroWhatsapp').value;
+  const area = document.getElementById('area').value;
+  const errorWhatsapp = document.getElementById('errorWhatsapp');
+  const mail = document.getElementById('mail').value;  
+  const errorMail = document.getElementById('errorMail');
+  const fechaSalida = document.getElementById('fechaSalida').value;  
+  const fechaVuelta = document.getElementById('fechaVuelta').value;  
+  const errorFechas = document.getElementById('errorFechas');
+  // AGREGAR RUTA Y CATEGORIA
+
+  const nombreValido = validarNombre(nombre, errorNombre);
+  const paisResidenciaValido = validarPaisResidencia(paisResidencia, errorPaisResidencia);
+  const numeroWhatsappValido = validarNumeroWhatsapp(numeroWhatsapp, area, errorWhatsapp);
+  const mailValido = validarMail(mail, errorMail);
+  const fechasValido = validarFechas(fechaSalida, fechaVuelta, errorFechas);
+  // Validar otros campos y almacenar los resultados
+
+  return nombreValido && paisResidenciaValido && numeroWhatsappValido && mailValido && fechasValido;
+}
+
+document.getElementById('vueloHotel').addEventListener('submit', function (event) {
+  event.preventDefault();
+  if (validarFormulario()) {
+    handleSubmit(event);
+  }
+});
 
 function handleSubmit(event) {
   event.preventDefault();
 
   const formData = {
     nombre: document.getElementById('nombre').value,
+    paisResidencia: document.getElementById('paisResidencia').value,
+    area: document.getElementById('area').value,
+    numeroWhatsapp: document.getElementById('numeroWhatsapp').value,
+    mail: document.getElementById('mail').value,
+    fechaSalida: document.getElementById('fechaSalida').value,
+    fechaVuelta: document.getElementById('fechaVuelta').value,
+
   };
   console.log(formData);
+  vaciarCampos();
+};
+
+function vaciarCampos() {
+  document.getElementById('nombre').value = '';
+  // Vaciar paisResidencia
+  document.getElementById('area').value = '';
+  document.getElementById('numeroWhatsapp').value = '';
+  document.getElementById('mail').value = '';
+  document.getElementById('fechaSalida').value = '';
+  document.getElementById('fechaVuelta').value = '';
 }
-
-function validateForm() {
-  const errorIcon = document.createElement('div');
-  errorIcon.className = 'errorIcon';
-  const errorText = document.createElement('p');
-
-  const nombre = document.getElementById('nombre').value;
-  const errorNombre = document.getElementById('errorNombre');
-  // Agregar los otros campos
-
-  // Agregar logica/s de los errores para cada campo
-  if (nombre.trim() === '') {
-    errorText.textContent = 'El nombre es requerido';
-    errorText.className = 'error'
-    errorNombre.appendChild(errorIcon);
-    errorNombre.appendChild(errorText);
-    return false;
-  } else {
-    errorNombre.textContent = '';
-  }
-  return true;
-}
-
-document.getElementById('vueloHotel').addEventListener('submit', function (event) {
-  if (!validateForm()) {
-    event.preventDefault();
-  } else {
-    handleSubmit(event);
-  }
-});
-
 
 $(document).ready(function () {
   $('.selectPaisResidencia').select2();
